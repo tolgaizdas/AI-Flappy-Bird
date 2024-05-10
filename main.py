f8fafab56b6cec7da6becf074b1880a85264bb5b
Tolga İzdaş <tolgaizdas@gmail.com>
2024-05-10 10:09:41 +0300

    minor modification

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from game import Game
 
 if __name__ == '__main__':
-    game = Game(ai=True, load=True)
+    ai = True
+    load = True
+    game = Game(ai, load)
     game.run()
-    if input("Do you want to save the Q-table? (y/n): ") == 'y':
+    if ai and input("Do you want to save the Q-table? (y/n): ") == 'y':
         game.save_q_table()
